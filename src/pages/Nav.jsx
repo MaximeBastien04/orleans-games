181199f4fb10a9596d31ed71c142f5126018978b
Maxime Bastien <caramelstudents@gmail.com>
2025-03-03 15:52:53 +0100

    feat: blurry nav background added

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -4,10 +4,13 @@ import { HashLink } from "react-router-hash-link";
 const Nav = () => {
     return (
         <nav>
-            <HashLink smooth to="/#home">Home</HashLink>
-            <HashLink smooth to="/#about">About Us</HashLink>
-            <HashLink smooth to="/#contact">Contact</HashLink>
-            <Link to="/games">Games</Link>
+            <ul>
+                <HashLink smooth to="/#home">Home</HashLink>
+                <HashLink smooth to="/#about">About Us</HashLink>
+                <HashLink smooth to="/#contact">Contact</HashLink>
+                <Link to="/games">Games</Link>
+            </ul>
+            <div></div>
         </nav>
     )
 };
